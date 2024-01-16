f07416940150ee0706c174f9d7d68d263a935cce
heryfitiavana22 <hheryfitiavana@gmail.com>
2024-01-16 16:40:13 +0300

    test force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,11 +18,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            URL::forceSchema('https');
         }
     }
 }
